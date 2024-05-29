8818e14cd2f3da63777605f213d6319e5dbdf257
bonarson <hei.bonarson@gmail.com>
2024-05-29 13:34:57 +0300

    feat: add getAllCar() controller

--- src/main/java/com/backend/web3/car/demo/backend/car/web3/controller/CarController.java
+++ src/main/java/com/backend/web3/car/demo/backend/car/web3/controller/CarController.java
@@ -3,7 +3,11 @@ package com.backend.web3.car.demo.backend.car.web3.controller;
 import com.backend.web3.car.demo.backend.car.web3.model.Car;
 import com.backend.web3.car.demo.backend.car.web3.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +24,11 @@ public class CarController {
     @GetMapping("/allCar")
     public List<Car> getAllCar() {
         return carService.getAllCar();
+    }
+
+    @PostMapping("/carAdd")
+    public ResponseEntity<Void> addCar(@RequestBody Car car) {
+        carService.addCar("add car done", car);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
