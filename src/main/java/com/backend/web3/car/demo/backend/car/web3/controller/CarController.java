d24c3b2680f6a6cdc40038c4290d36d18bd7e4b1
bonarson <hei.bonarson@gmail.com>
2024-05-29 12:58:28 +0300

    feat:add CarController and implements method getAllCar

--- src/main/java/com/backend/web3/car/demo/backend/car/web3/controller/CarController.java
+++ src/main/java/com/backend/web3/car/demo/backend/car/web3/controller/CarController.java
@@ -1,5 +1,24 @@
 package com.backend.web3.car.demo.backend.car.web3.controller;
 
-public class CarController {
+import com.backend.web3.car.demo.backend.car.web3.model.Car;
+import com.backend.web3.car.demo.backend.car.web3.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/allCar")
+    public List<Car> getAllCar() {
+        return carService.getAllCar();
+    }
 }
