debd45d5427a68d1543cc19543acb4078d7e1262
bonarson <hei.bonarson@gmail.com>
2024-05-29 12:01:01 +0300

    feat : add CarRepository and methode getAllCar()

---
+++ src/main/java/com/backend/web3/car/demo/backend/car/web3/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.backend.web3.car.demo.backend.car.web3.repository;
+
+import com.backend.web3.car.demo.backend.car.web3.model.Car;
+
+import java.util.List;
+
+public interface CarRepository {
+    List<Car> getAllCar();
+
+}
