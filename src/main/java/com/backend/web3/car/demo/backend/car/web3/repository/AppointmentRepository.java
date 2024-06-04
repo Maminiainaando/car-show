03dd5a48f24405b3c3b0b9e6aa515b64c81f03ce
bonarson <hei.bonarson@gmail.com>
2024-06-04 21:53:48 +0300

    feat:gerAllAppointment in repository

--- src/main/java/com/backend/web3/car/demo/backend/car/web3/repository/AppointmentRepository.java
+++ src/main/java/com/backend/web3/car/demo/backend/car/web3/repository/AppointmentRepository.java
@@ -2,6 +2,9 @@ package com.backend.web3.car.demo.backend.car.web3.repository;
 
 import com.backend.web3.car.demo.backend.car.web3.model.Appointment;
 
+import java.util.List;
+
 public interface AppointmentRepository {
+    List<Appointment> getAllAppointment();
     void addAppointment(String message, Appointment appointment);
 }
