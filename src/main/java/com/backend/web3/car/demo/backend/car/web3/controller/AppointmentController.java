b34428b1a2ab25974299d706eac9be13eb1f557d
bonarson <hei.bonarson@gmail.com>
2024-06-04 21:55:33 +0300

    feat: /allAppointment

--- src/main/java/com/backend/web3/car/demo/backend/car/web3/controller/AppointmentController.java
+++ src/main/java/com/backend/web3/car/demo/backend/car/web3/controller/AppointmentController.java
@@ -1,19 +1,30 @@
 package com.backend.web3.car.demo.backend.car.web3.controller;
 
 import com.backend.web3.car.demo.backend.car.web3.model.Appointment;
+
 import com.backend.web3.car.demo.backend.car.web3.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AppointmentController {
     private final AppointmentService appointmentService;
 
+    @Autowired
     public AppointmentController(AppointmentService appointmentService) {
         this.appointmentService = appointmentService;
+    }
+
+    @GetMapping("/allAppointment")
+    public List<Appointment> getAllAppointment() {
+        return appointmentService.getAllAppointment();
     }
 
     @PostMapping("/addAppointment")
